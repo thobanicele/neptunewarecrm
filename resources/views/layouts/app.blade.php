3ba911e964056cde661d89253438a8ba1a041107
thobanicele <musa@neptuneware.com>
2026-02-25 17:58:53 +0200

    Updated Meta Data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-    <meta name="author" content="AdminKit">
+    <meta name="description" content="A modern, multi-tenant CRM built for teams that need leads, deals, quotes, invoices and payments in one place.">
+    <meta name="author" content="NeptuneWare Pty LTD">
     <meta name="keywords"
-        content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+        content="A modern, multi-tenant CRM built for teams that need leads, deals, quotes, invoices and payments in one place.">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{ asset('asset/img/icons/icon-48x48.png') }}" />
