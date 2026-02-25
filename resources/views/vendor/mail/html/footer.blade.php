b358a0fd1530e65c3a81eaba24dd44d87c89b042
thobanicele <musa@neptuneware.com>
2026-02-25 12:21:49 +0200

    Changed Email template styling

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,12 @@
+<tr>
+    <td class="footer">
+        <p style="margin: 0 0 8px 0;">
+            © {{ date('Y') }} NeptuneWare CRM. All rights reserved.
+        </p>
+        <p style="margin: 0;">
+            <a href="https://crm.neptuneware.com/terms-of-service">Terms</a> ·
+            <a href="https://crm.neptuneware.com/privacy-policy">Privacy</a> ·
+            <a href="https://crm.neptuneware.com/support">Support</a>
+        </p>
+    </td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <a href="{{ config('app.url') }}" style="display: inline-block;">
+            <img src="https://crm.neptuneware.com/asset/img/Raster_1024x1024_Transparent.png"
+                alt="{{ config('app.name') }}" height="42" style="height:42px; max-height:42px;">
+        </a>
+    </td>
+</tr>
